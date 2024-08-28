d5e130ee12412460da2e3818a7874f6e68424123
Mario Gonzalez <gonzalez.mario1997@gmail.com>
2024-08-28 01:40:47 -0400

    add google

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      providerOptions: {
+        serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
+        bucketName: env("GCS_BUCKET_NAME"),
+        basePath: env("GCS_BASE_PATH"),
+        baseUrl: env("GCS_BASE_URL"),
+        publicFiles: env("GCS_PUBLIC_FILES"),
+        uniform: env("GCS_UNIFORM"),
+      },
+    },
+  },
+  //...
+});
